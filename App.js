225f06dc00da1177816c856813d40398a575e018
Shivraj Singh Chauhan <shivrajsinghchauhan@Shivrajs-MacBook-Air.local>
2025-07-28 14:55:21 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,22 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",
-    { id: "parent" },
-    [
-        React.createElement("div", { id: "child1" }, [
-            React.createElement("h1", {}, "This is namaste react 🥰"),
-            React.createElement("h2", {}, "I am an h2 tag")
-        ]
-        ),
-        React.createElement("div", { id: "child2" },
-            [
-                React.createElement("h1", {}, "I am an h1 tag"),
-                React.createElement("h2", {}, "I am an h2 tag")
-            ])
-    ]);
+// React.createElement => ReactElement - JS object => HTMLElement(render)
 
-console.log(parent);
+// JSX => JSX is a syntax extension for JavaScript that looks like HTML.
+
+// JSX (transpiled before it reaches the JavaScrit Engine) - transpiled is done by PARCEL - Babel(Its a JS package which convert the code that React or browser can understand.)
+
+// JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLElement(render)
+
+
+// React Element
+const heading = (
+<h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+    </h1>
+    );
+
+const Title = () => (
+<h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+    <title />
+    </h1>
+    );
+
+
+// React Component 
+// Class based Component - OLD - No body used in modern React
+// Functional based Component- NEW
+
+
+// Functional based Component
+
+const HeadingComponent = () => (
+    <div id = "container">
+        {Title()}
+        <Title/ >
+        <Title></Title>
+      <h1 className="heading">Namaste React Functional Component</h1>
+ </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+// root.render(heading);
+root.render(<HeadingComponent />);
+
+    
